f59fe049bebf658fde24075ce4169d82e8b78d13
abdullahyusuf9805 <abdullahyusuf9805@gmail.com>
2026-08-19 16:48:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -250,11 +250,13 @@ st.markdown(
     [data-testid="stSidebar"] [data-testid="stMarkdownContainer"] p,
     [data-testid="stSidebar"] [data-testid="stMarkdownContainer"] h1,
     [data-testid="stSidebar"] [data-testid="stMarkdownContainer"] h2,
-    [data-testid="stSidebar"] [data-testid="stMarkdownContainer"] h3 {{
+    [data-testid="stSidebar"] [data-testid="stMarkdownContainer"] h3 {
         margin-top: 0 !important;
         margin-bottom: 0 !important;
+        padding-top: 0 !important;
+        padding-bottom: 0 !important;
         line-height: 1.3 !important;
-    }}
+    }
     </style>
     """,
     unsafe_allow_html=True
