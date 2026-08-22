0e3ad51c9244c4f8e288faef4d180471b456d492
abdullahyusuf9805 <abdullahyusuf9805@gmail.com>
2026-08-23 02:08:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1372,13 +1372,14 @@ else:
         code_val = s.get('code', '')
 
         excel_rows_html += "<tr>"
-        excel_rows_html += f'<td style="border: 1px solid #333333; padding: 12px 10px; text-align: center !important; vertical-align: middle !important; border-radius: 0px !important;">{status_val}</td>'
-        excel_rows_html += f'<td style="border: 1px solid #333333; padding: 12px 10px; text-align: center !important; vertical-align: middle !important; border-radius: 0px !important;">{teacher_val}</td>'
-        excel_rows_html += f'<td style="border: 1px solid #333333; padding: 12px 10px; text-align: center !important; vertical-align: middle !important; border-radius: 0px !important;">{venue_val}</td>'
-        excel_rows_html += f'<td style="border: 1px solid #333333; padding: 12px 10px; text-align: center !important; vertical-align: middle !important; border-radius: 0px !important;">{hall_val}</td>'
-        excel_rows_html += f'<td style="border: 1px solid #333333; padding: 12px 10px; text-align: center !important; vertical-align: middle !important; border-radius: 0px !important;">{id_val}</td>'
-        excel_rows_html += f'<td style="border: 1px solid #333333; padding: 12px 10px; text-align: center !important; vertical-align: middle !important; border-radius: 0px !important;">{name_val}</td>'
+        # Right-to-left order: الحالة -> المحاضر -> الوقت -> رقم القاعة -> رقم الشعبة -> المقرر -> رمز المقرر
         excel_rows_html += f'<td style="border: 1px solid #333333; padding: 12px 10px; text-align: center !important; vertical-align: middle !important; border-radius: 0px !important;">{code_val}</td>'
+        excel_rows_html += f'<td style="border: 1px solid #333333; padding: 12px 10px; text-align: center !important; vertical-align: middle !important; border-radius: 0px !important;">{name_val}</td>'
+        excel_rows_html += f'<td style="border: 1px solid #333333; padding: 12px 10px; text-align: center !important; vertical-align: middle !important; border-radius: 0px !important;">{id_val}</td>'
+        excel_rows_html += f'<td style="border: 1px solid #333333; padding: 12px 10px; text-align: center !important; vertical-align: middle !important; border-radius: 0px !important;">{hall_val}</td>'
+        excel_rows_html += f'<td style="border: 1px solid #333333; padding: 12px 10px; text-align: center !important; vertical-align: middle !important; border-radius: 0px !important;">{venue_val}</td>'
+        excel_rows_html += f'<td style="border: 1px solid #333333; padding: 12px 10px; text-align: center !important; vertical-align: middle !important; border-radius: 0px !important;">{teacher_val}</td>'
+        excel_rows_html += f'<td style="border: 1px solid #333333; padding: 12px 10px; text-align: center !important; vertical-align: middle !important; border-radius: 0px !important;">{status_val}</td>'
         excel_rows_html += "</tr>"
 
     excel_table_html = f"""
@@ -1386,13 +1387,13 @@ else:
         <table dir="rtl" style="width: 100%; border-collapse: collapse; font-family: sans-serif; font-size: 14px; background-color: #121212; color: #ffffff; border-radius: 0px !important;">
             <thead>
                 <tr style="background-color: #212121; color: #ffffff;">
-                    <th style="border: 1px solid #333333; padding: 12px 10px; text-align: center !important; vertical-align: middle !important; border-radius: 0px !important;">الحالة</th>
-                    <th style="border: 1px solid #333333; padding: 12px 10px; text-align: center !important; vertical-align: middle !important; border-radius: 0px !important;">المحاضر</th>
-                    <th style="border: 1px solid #333333; padding: 12px 10px; text-align: center !important; vertical-align: middle !important; border-radius: 0px !important;">الوقت</th>
-                    <th style="border: 1px solid #333333; padding: 12px 10px; text-align: center !important; vertical-align: middle !important; border-radius: 0px !important;">رقم القاعة</th>
-                    <th style="border: 1px solid #333333; padding: 12px 10px; text-align: center !important; vertical-align: middle !important; border-radius: 0px !important;">رقم الشعبة</th>
-                    <th style="border: 1px solid #333333; padding: 12px 10px; text-align: center !important; vertical-align: middle !important; border-radius: 0px !important;">المقرر</th>
                     <th style="border: 1px solid #333333; padding: 12px 10px; text-align: center !important; vertical-align: middle !important; border-radius: 0px !important;">رمز المقرر</th>
+                    <th style="border: 1px solid #333333; padding: 12px 10px; text-align: center !important; vertical-align: middle !important; border-radius: 0px !important;">المقرر</th>
+                    <th style="border: 1px solid #333333; padding: 12px 10px; text-align: center !important; vertical-align: middle !important; border-radius: 0px !important;">رقم الشعبة</th>
+                    <th style="border: 1px solid #333333; padding: 12px 10px; text-align: center !important; vertical-align: middle !important; border-radius: 0px !important;">رقم القاعة</th>
+                    <th style="border: 1px solid #333333; padding: 12px 10px; text-align: center !important; vertical-align: middle !important; border-radius: 0px !important;">الوقت</th>
+                    <th style="border: 1px solid #333333; padding: 12px 10px; text-align: center !important; vertical-align: middle !important; border-radius: 0px !important;">المحاضر</th>
+                    <th style="border: 1px solid #333333; padding: 12px 10px; text-align: center !important; vertical-align: middle !important; border-radius: 0px !important;">الحالة</th>
                 </tr>
             </thead>
             <tbody>
@@ -1440,7 +1441,7 @@ else:
             mime="text/csv",
             use_container_width=True
         )
-
+    
     
     st.markdown("---")
     st.markdown('<div class="center-download">', unsafe_allow_html=True)
